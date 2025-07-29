96c48c8c3f5c48368384349a42c9e252267b62f9
atharvgawande31 <atharvdgawande@gmail.com>
2025-07-29 22:48:18 +0530

    Final cursor animation that scales on hovering over the image

--- GSAP _Cursor_Animation/index.js
+++ GSAP _Cursor_Animation/index.js
@@ -1,5 +1,6 @@
 let cursor = document.querySelector(".cursor")
 let main = document.querySelector(".main")
+let images = document.querySelector(".image")
 
 main.addEventListener("mousemove", function(e) {
 gsap.to(cursor, {
@@ -7,8 +8,20 @@ gsap.to(cursor, {
    y: e.y,
    duration: 1,
    ease: "back.out"
-
-
 })   
 })
 
+
+images.addEventListener("mouseenter", function() {
+   cursor.innerHTML = "View More"
+   gsap.to(cursor, {
+      scale: 2
+   });
+});
+
+images.addEventListener("mouseleave", function() {
+   cursor.innerHTML = ""
+   gsap.to(cursor, {
+      scale: 1
+   });
+});
